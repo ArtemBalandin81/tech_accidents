47b98e5e7f030626fedcd8e1640b94d6793b3cd5
unknown <@BalandinArtem.Agidel.ru>
2023-09-19 20:28:22 +0500

    feature/add_correct_time_zone_shift

--- src/settings.py
+++ src/settings.py
@@ -1,9 +1,12 @@
 """src/settings.py"""
+from functools import lru_cache
 from pathlib import Path
 from urllib.parse import urljoin
 
-from pydantic import BaseSettings, validator
-from pydantic.tools import lru_cache
+#from pydantic import BaseSettings, validator  # УСТАРЕЛО!
+from pydantic import validator
+from pydantic_settings import BaseSettings
+# from pydantic.tools import lru_cache  # УСТАРЕЛО!
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -41,6 +44,13 @@ class Settings(BaseSettings):
     # DB_HOST: str = "localhost"
     # DB_PORT: int = 5432
 
+    # Настройки логирования
+    LOG_LEVEL: str = "INFO"
+    LOG_DIR: str | Path = BASE_DIR / "logs"
+    LOG_FILE: str = "app.log"
+    LOG_FILE_SIZE: int = 10 * 2**20
+    LOG_FILES_TO_KEEP: int = 5
+
     @property
     def database_url(self) -> str:
         """Получить ссылку для подключения к DB."""
@@ -51,26 +61,20 @@ class Settings(BaseSettings):
         # )
         return "sqlite+aiosqlite:///./tech_accident_db_local.db"
 
-    class Config:
-        #env_file = get_env_path()
-        env_file = '.env'
+    # class Config:  # Устарело TODO убрать
+    #     #env_file = get_env_path()
+    #     env_file = '.env'
 
     # Настройки бота
     #BOT_TOKEN: str
     #BOT_WEBHOOK_MODE: bool = False
 
-    # Настройки логирования
-    # LOG_LEVEL: str = "INFO"
-    # LOG_DIR: str | Path = BASE_DIR / "logs"
-    # LOG_FILE: str = "app.log"
-    # LOG_FILE_SIZE: int = 10 * 2**20
-    # LOG_FILES_TO_KEEP: int = 5
 
 
 @lru_cache()
 def get_settings():
-    return Settings()
-
+    # return Settings()  # Устарело
+    return Settings(_env_file=get_env_path())  # type: ignore
 
 
 settings = get_settings()
