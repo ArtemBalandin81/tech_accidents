7a389e44655c9f321ce096461b79d1a4a9ade8ce
Артем Баландин <tom9981@yandex.ru>
2023-10-01 23:26:09 +0500

    feature/add_run_test_connection_time_counter

--- src/services/test_connection.py
+++ src/services/test_connection.py
@@ -5,6 +5,7 @@ from time import sleep
 from datetime import datetime
 
 from src.api.constants import CONNECTION_TEST_URL
+SLEEP = 5
 
 
 async def test_connection(CONNECTION_TEST_URL: str = "https://www.agidel-am.ru") -> dict[str, int | str]:  # TODO -> ???:
@@ -15,25 +16,28 @@ async def test_connection(CONNECTION_TEST_URL: str = "https://www.agidel-am.ru")
     return result
 
 
-async def run_test_connection_asyncio():  #TODO после сбоя не перезапускается
+async def run_test_connection_asyncio(time_counter: int = 0):  #TODO после сбоя не перезапускается
     """ Запускает периодический процесс тестирование доступа к интернет."""
-    counter = 0
     try:
         while True:
-            await asyncio.sleep(5)
+            await asyncio.sleep(SLEEP)
             await test_connection()
+            if time_counter != 0:
+                print(f"простой составил: {time_counter}")
+                # TODO фиксируется время простоя и заносится в БД
+                # TODO нужно фиксировать время начало простоя и окончания
+                time_counter = 0  # обнуляем счетчик, если соединение восстановилось
     except requests.exceptions.ConnectionError:
         print(f"connection_error: {ConnectionError}")
-        await asyncio.sleep(5)
-        await run_test_connection_asyncio() #TODO счетчик простоя
+        time_counter += SLEEP
+        print(f"time_counter: {time_counter}")
+        await asyncio.sleep(SLEEP)
+        await run_test_connection_asyncio(time_counter)  # рекурсией проверяем восстановление соединения + счетчик
 
-        # counter += 1
-        # print(f"counter: {counter}")
 
-
-        # 1. Запускаем счетчик секунд.  #TODO счетчик простоя
+        # 1. Запускаем счетчик секунд.
         # 2. Через каждые 5 сек проверяется связь
-        #    2.1 Если связь есть - фиксируется время простоя и заносится в БД
+        #    2.1 Если связь есть - #TODO фиксируется время простоя и заносится в БД
         #    2.2 Если связи нет, то счетчик продолжает бежать
         # 3. Нужно каким-то образом перезапускать run_test_connection_asyncio после обрыва связи
 
