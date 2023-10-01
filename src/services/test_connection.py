b4b270b25939a3f7b9b5265a0f565ddabacd062b
Артем Баландин <tom9981@yandex.ru>
2023-10-01 20:06:27 +0500

    feature/add_run_test_connection_asyncio_add_on_off

--- src/services/test_connection.py
+++ src/services/test_connection.py
@@ -15,11 +15,37 @@ async def test_connection(CONNECTION_TEST_URL: str = "https://www.agidel-am.ru")
     return result
 
 
-async def run_test_connection_asyncio():
+async def run_test_connection_asyncio():  #TODO после сбоя не перезапускается
     """ Запускает периодический процесс тестирование доступа к интернет."""
-    while True:
+    counter = 0
+    try:
+        while True:
+            await asyncio.sleep(5)
+            await test_connection()
+    except requests.exceptions.ConnectionError:
+        print(f"connection_error: {ConnectionError}")
         await asyncio.sleep(5)
-        await test_connection()
+        await run_test_connection_asyncio() #TODO счетчик простоя
+
+        # counter += 1
+        # print(f"counter: {counter}")
+
+
+        # 1. Запускаем счетчик секунд.  #TODO счетчик простоя
+        # 2. Через каждые 5 сек проверяется связь
+        #    2.1 Если связь есть - фиксируется время простоя и заносится в БД
+        #    2.2 Если связи нет, то счетчик продолжает бежать
+        # 3. Нужно каким-то образом перезапускать run_test_connection_asyncio после обрыва связи
+
+
+
+    #requests.exceptions.ConnectionError
+    # while True:
+    #     await asyncio.sleep(5)
+    #     await test_connection()
+
+
+
 
 # async def run_test_connection():  #TODO УДАЛИТЬ
 #     """ Запускает периодический процесс тестирование доступа к интернет."""
