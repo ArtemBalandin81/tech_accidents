0c5d4d8a245123cb2054268a89a1978d56fbde59
Артем Баландин <tom9981@yandex.ru>
2023-11-25 10:56:30 +0500

    feature/fix_user_id_to_email

--- src/api/schemas.py
+++ src/api/schemas.py
@@ -2,7 +2,7 @@
 from datetime import datetime, timedelta
 
 from fastapi_users import schemas
-from pydantic import BaseModel, Extra, Field, field_serializer, root_validator, validator
+from pydantic import BaseModel, EmailStr, Extra, Field, field_serializer, computed_field, root_validator, validator
 
 from src.core.enums import RiskAccidentSource, TechProcess
 from typing_extensions import TypedDict
@@ -11,43 +11,43 @@ from .constants import DATE_TIME_FORMAT, FROM_TIME, TO_TIME
 from src.settings import settings
 
 
-class SuspensionBase(BaseModel):
-    """Базовая схема."""
+# https://github.com/fastapi-users/fastapi-users/blob/master/fastapi_users/schemas.py
+class UserRead(schemas.BaseUser[int]):
+    """Pydantic schemas FastApi Users."""
+    # id: models.ID
+    # email: EmailStr
+    # is_active: bool = True
+    # is_superuser: bool = False
+    # is_verified: bool = False
+    pass
+
+
+class UserCreate(schemas.BaseUserCreate):
+    """Pydantic schemas FastApi Users."""
+    # email: EmailStr
+    # password: str
+    # is_active: Optional[bool] = True
+    # is_superuser: Optional[bool] = False
+    # is_verified: Optional[bool] = False
+    pass
+
+
+class UserUpdate(schemas.BaseUserUpdate):
+    """Pydantic schemas FastApi Users."""
+    # password: Optional[str] = None
+    # email: Optional[EmailStr] = None
+    # is_active: Optional[bool] = None
+    # is_superuser: Optional[bool] = None
+    # is_verified: Optional[bool] = None
+    pass
+
+
+class SuspensionRequest(BaseModel):  # TODO реализовать схему валидации
+    """Схема json-запроса для создания Suspension."""
     datetime_start: datetime = Field(..., example=FROM_TIME)
     datetime_finish: datetime = Field(..., example=TO_TIME)
     description: str = Field(..., max_length=256, example="Сбой подключения к интернет.")
     implementing_measures: str = Field(..., max_length=256, example="Перезагрузка оборудования.")
-
-    class Config:
-        """Implement a custom json serializer by using pydantic's custom json encoders.
-        Переводит datetime из БД в формат str для удобства отображения."""
-        json_encoders = {datetime: lambda db_date_time: (db_date_time + timedelta(hours=0)).strftime(DATE_TIME_FORMAT)}
-
-class SuspensionResponse(SuspensionBase):
-    """Схема ответа для Suspension."""
-    risk_accident: str
-    tech_process: int  # TODO сериализовать в "25 - ДУ"
-    user_id: int  #TODO сериализовать в "user_name_surname"
-    created_at: datetime
-    updated_at: datetime
-    id: int
-
-    @field_serializer("datetime_start", "datetime_finish", "created_at", "updated_at")
-    def serialize_server_time_to_time_shift(self, server_time: datetime, _info):
-        """Отображает сохраненное время сервера с требуемым сдвигом."""
-        return server_time.strftime(DATE_TIME_FORMAT)
-
-    @field_serializer("created_at", "updated_at")
-    def serialize_server_time_to_time_shift(self, server_time: datetime, _info):
-        """Отображает сохраненное время сервера с требуемым сдвигом."""
-        return (server_time + timedelta(hours=settings.TIMEZONE_OFFSET)).strftime(DATE_TIME_FORMAT)
-
-    class Config:
-        from_attributes = True  # in V2: 'orm_mode' has been renamed to 'from_attributes'
-
-
-class SuspensionRequest(SuspensionBase):  # TODO реализовать схему валидации
-    """Схема json-запроса для создания Suspension."""
     risk_accident: RiskAccidentSource
     tech_process: TechProcess
 
@@ -65,35 +65,67 @@ class SuspensionRequest(SuspensionBase):  # TODO реализовать схем
         }
 
 
-class TotalTimeSuspensions(TypedDict):
-    """Класс ответа для аналитики за период времени."""
+class SuspensionBase(BaseModel):
+    """Базовая схема."""
+    id: int
+    datetime_start: datetime = Field(..., example=FROM_TIME)
+    datetime_finish: datetime = Field(..., example=TO_TIME)
+    description: str = Field(..., max_length=256, example="Сбой подключения к интернет.")
+    implementing_measures: str = Field(..., max_length=256, example="Перезагрузка оборудования.")
 
-    total_time_suspensions_in_mins: int
+    class Config:
+        """Implement a custom json serializer by using pydantic's custom json encoders.
+        Переводит datetime из БД в формат str для удобства отображения."""
+        json_encoders = {datetime: lambda db_date_time: (db_date_time + timedelta(hours=0)).strftime(DATE_TIME_FORMAT)}
+
+
+class SuspensionResponse(SuspensionBase):
+    """Схема ответа для Suspension с использованием response_model."""
+    risk_accident: str
+    tech_process: int
+    user_id: int
+    created_at: datetime
+    updated_at: datetime
+
+    @field_serializer("datetime_start", "datetime_finish", "created_at", "updated_at")
+    def serialize_server_time_to_time_shift(self, server_time: datetime, _info):
+        """Отображает сохраненное время сервера с требуемым сдвигом."""
+        return server_time.strftime(DATE_TIME_FORMAT)
+
+    @field_serializer("created_at", "updated_at")
+    def serialize_server_time_to_time_shift(self, server_time: datetime, _info):
+        """Отображает сохраненное время сервера с требуемым сдвигом."""
+        return (server_time + timedelta(hours=settings.TIMEZONE_OFFSET)).strftime(DATE_TIME_FORMAT)
+
+    class Config:
+        from_attributes = True  # in V2: 'orm_mode' has been renamed to 'from_attributes'
+
+
+class AnalyticResponse(SuspensionBase):
+    """Схема ответа для аналитики."""
+    risk_accident: str
+    # tech_process: int
+    business_process: str
+    user_email: str
+    user_id: int
+    created_at: datetime
+    updated_at: datetime
+
+    class Config:
+        from_attributes = True  # in V2: 'orm_mode' has been renamed to 'from_attributes'
 
 
 class SuspensionAnalytics(BaseModel):
     """Класс ответа для аналитики случаев простоя за период времени."""
 
-    #time_suspensions: TotalTimeSuspensions = {}
     suspensions_in_mins_total: int
     suspensions_total: int
     max_suspension_time_for_period: int
     last_time_suspension: datetime
     last_time_suspension_id: int
-    suspensions: list[SuspensionResponse] = {}
+    # suspensions: list[SuspensionResponse] = {}
+    suspensions_detailed: list[AnalyticResponse] = {}
 
     class Config:
         from_attributes = True  # in V2: 'orm_mode' has been renamed to 'from_attributes'
         json_encoders = {datetime: lambda db_date_time: db_date_time.strftime(DATE_TIME_FORMAT)}
-
-
-class UserRead(schemas.BaseUser[int]):
-    pass
-
-
-class UserCreate(schemas.BaseUserCreate):
-    pass
-
-
-class UserUpdate(schemas.BaseUserUpdate):
-    pass
--- src/api/services/__init__.py
+++ src/api/services/__init__.py
@@ -1,7 +1,9 @@
 from .base import ContentService
 from .suspension import SuspensionService
+from .users import UsersService
 
 __all__ = (
     "ContentService",
     "SuspensionService",
+    "UsersService"
 )
