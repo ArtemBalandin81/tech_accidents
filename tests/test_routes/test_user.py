4c253e8ceecde6d54e53db59c70c20d72c5e0575
Артем Баландин <tom9981@yandex.ru>
2024-07-30 20:09:21 +0500

    feature/add_first_tests_user_unauthorized

--- tests/test_routes/test_user.py
+++ tests/test_routes/test_user.py
@@ -27,16 +27,50 @@ from src.settings import settings
 pytestmark = pytest.mark.anyio  # make all test mark with `anyio`
 
 
-#@pytest.mark.anyio  # pytestmark = pytest.mark.anyio  == make all test mark with `anyio`
+# @pytest.mark.anyio  # pytestmark = pytest.mark.anyio  == make all test mark with `anyio`
 async def test_test_url(async_client):
     """Тестирует сервисный эндпоинт проверки доступности сайтов: __/api/services/test_url__"""
     url = "https://agidel-am.ru/"
     api_url = "/api/services/test_url"
     async with async_client as ac:
         response = await ac.get(api_url, params={"url": url})
-    assert response.status_code == 200
+    assert response.status_code == 200, f"api: test_url status code {response.status_code} is not 200"
     await log.ainfo("test_info:", response_json=response.json(), url=response.url)
-    # pprint(f'response_json: {response.json()}')
+    pprint(f'response_json: {response.json()}')
+
+# todo в цикле, словаре сделать тест нескольких эндпоинтов неавторизованным пользователем
+# todo сделай красиво в цикле по словарю как было в спринте, постарайся!!!
+async def test_unauthorized_get_urls(async_client: AsyncClient, async_db: AsyncSession) -> None:
+    """Тестирует доступ к пользовательским эндпоинтам неавторизованным пользователем"""
+    data = {"email": "testuser@nofoobar.com", "password": "testing_testing"}
+    users_me_url = "/api/users/me"
+    test_url = "/api/services/test_url"
+    db_backups_url = "/api/services/db_backup"
+    a = "/api/auth/jwt/login"  # todo
+    b = "/api/auth/jwt/logout"  # todo + проверить регом
+    c_get = "/api/users/{id}"  # todo + проверить регом
+    b_patch = "/api/users/{id}"  # todo + проверить регом
+    e = "/api/users"  # todo + проверить админом!!!
+
+    async with async_client as ac:
+        # response = await ac.patch(api_url, json=data)
+        response_get_users_me = await ac.get(users_me_url)
+        response_patch_users_me = await ac.patch(users_me_url, json=data)
+        response_test_url = await ac.get(test_url, params={"url": "https://agidel-am.ru/"})
+        response_db_backups = await ac.get(db_backups_url)
+
+    assert response_get_users_me.status_code == 401, f"Unauthorized get {users_me_url} but shouldn't"
+    await log.ainfo("get_users_me", response_json=response_get_users_me.json(), url=response_get_users_me.url)
+
+    assert response_patch_users_me.status_code == 401, f"Unauthorized patch {users_me_url} but shouldn't"
+    await log.ainfo("patch_users_me", response_json=response_patch_users_me.json(), url=response_patch_users_me.url)
+
+    assert response_test_url.status_code == 200, f"Unauthorized couldn't get {test_url} but should"
+    await log.ainfo("get_test_url", response_json=response_test_url.json(), url=response_test_url.url)
+
+    assert response_db_backups.status_code == 401, f"Unauthorized get {db_backups_url} but shouldn't"
+    await log.ainfo("get_db_backups", response_json=response_db_backups.json(), url=response_db_backups.url)
+
 
 
 async def test_create_user(async_client: AsyncClient, async_db: AsyncSession) -> None:
@@ -47,14 +81,14 @@ async def test_create_user(async_client: AsyncClient, async_db: AsyncSession) ->
     api_url = "/api/auth/register"
     async with async_client as ac:
         response = await ac.post(api_url, json=data)
-    assert response.status_code == 201
+    assert response.status_code == 201, f"api: user register status code {response.status_code} is not 201"
     user_created = await async_db.scalar(select(User).where(User.email == email))
-    assert email == user_created.email
-    assert response.json()["id"] == user_created.id
-    assert response.json()["is_superuser"] == user_created.is_superuser
-    assert user_created.is_superuser is False
+    assert email == user_created.email, f"created user {user_created.email} doesn't meet expectations: {email}"
+    assert response.json()["id"] == user_created.id, f"user id {user_created.id} doesn't meet {response.json()['id']}"
+    assert response.json()["is_superuser"] == user_created.is_superuser, "user is_superuser doesn't meet expectations"
+    assert user_created.is_superuser is False, f"user_created.is_superuser: {user_created.is_superuser} is not False"
     await log.ainfo(
-        "test_info:", response=response.json(), url=response.url,
+        "post_auth_register:", response=response.json(), url=response.url,
         user_email=user_created.email, user_id=user_created.id, super_user=user_created.is_superuser,
     )
     # pprint(f'response_json: {response.json()}')
@@ -63,10 +97,10 @@ async def test_user_and_super_user_exist(
         async_client: AsyncClient, async_db: AsyncSession, super_user_orm: User, user_orm: User
 ) -> None:
     """Тестирует, что в БД созданы супер-пользователь и пользователь."""
-    assert super_user_orm.is_superuser is True
-    assert super_user_orm.email == "super_user_fixture@nofoobar.com"
-    assert user_orm.is_superuser is False
-    assert user_orm.email == "user_fixture@nofoobar.com"
+    assert super_user_orm.is_superuser is True, "super_user_orm is not super_user but should"
+    assert super_user_orm.email == "super_user_fixture@nofoobar.com", "email of super_user_orm is not correct"
+    assert user_orm.is_superuser is False, "user_orm is super_user but shouldn't"
+    assert user_orm.email == "user_fixture@nofoobar.com", "email of user_orm doesn't meet expectations"
     await log.ainfo(
         "test_info:",
         super_user_email=super_user_orm.email,
@@ -78,21 +112,19 @@ async def test_user_and_super_user_exist(
     )
     # pprint(f'super_user_hashed_password: {super_user_orm.hashed_password}')
 
-# async def test_assign_super_user(async_client: AsyncClient, async_db: AsyncSession) -> None:  # todo test super_user
-#     """Тестирует эндпоинт патча пользователя: __???__"""
-#     data = {"email": "testuser@nofoobar.com", "password": "testing_testing"}
-#     api_url = "/api/auth/register"
-#     async with async_client as ac:
-#         response = await ac.post(api_url, json=data)
-#     assert response.status_code == 201
-#     await log.ainfo("response", response_json=response.json(), url=response.url)
-#     # pprint(f'response_json: {response.json()}')
+async def test_authorized_patch_users_me(async_client: AsyncClient, async_db: AsyncSession) -> None:
+    """Тестирует эндпоинт патча пользователя: __/api/users/me__"""
+    data = {"email": "testuser@nofoobar.com", "password": "testing_testing"}
+    api_url = "/api/users/me"
+    async with async_client as ac:
+        # response = await ac.patch(api_url, json=data)  # todo finish
+        response = await ac.get(api_url)  # todo finish
+    assert response.status_code == 401
+    await log.ainfo("patch_users_me", response_json=response.json(), url=response.url)
+    # pprint(f'response_json: {response.json()}')
 
 
-
-
-
-# def test_db_backup(async_client: AsyncClient):  # todo пока не дает, т.к. нет авторизованного пользователя
+# def test_authorized_get_db_backup(async_client: AsyncClient):  # todo нужен авторизованный пользователь
 #     """Тестирует сервисный эндпоинт бэкапа БД авторизованным пользователем: __/api/services/db_backup__"""
 #     api_url = "/api/services/db_backup"
 #     # response = client.get(api_url)
