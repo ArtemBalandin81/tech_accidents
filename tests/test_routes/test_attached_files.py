e58ccd1b759dfe781b76ad1754a3b2da5855d710
Артем Баландин <tom9981@yandex.ru>
2024-11-30 18:08:31 +0500

    feature/add_test_super_user_delete_file_id_url_near_finish_301124

--- tests/test_routes/test_attached_files.py
+++ tests/test_routes/test_attached_files.py
@@ -31,7 +31,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 from src.api.constants import *
-from src.core.db.models import FileAttached, Task, TasksFiles, User
+from src.core.db.models import FileAttached, Suspension, SuspensionsFiles, Task, TasksFiles, User
 from src.settings import settings
 
 from tests.conftest import clean_test_database, create_test_files, delete_files_in_folder, remove_all
@@ -469,10 +469,11 @@ async def test_super_user_delete_file_id_url(
     привязанный файл не должен удаляться
     pytest -k test_super_user_delete_file_id_url -vs
 
-    scenarios - тестовые сценарии использования эндпоинта (сценарии НЕ СВЯЗАНЫ!!!).
+    scenarios - тестовые сценарии использования эндпоинта (сценарии СВЯЗАНЫ!!!).
     expected - словарь ожидаемых значений в сценарии.
     match_values - кортеж параметров, используемых в assert (ожидание - реальность).
     """
+    # todo добавить респонсы в схемы ответа в апи!!!
     # todo - проверять привязку к suspension, но не suspensions_orm - т.к. там создаются повторно пользователи:
     # использовать async_db.add & async_db.commit
     test_url = FILES_PATH + "/"  # /api/files/{file_id}  # file_id лишнее и не используется в апи todo
@@ -486,39 +487,21 @@ async def test_super_user_delete_file_id_url(
         ("files", open(TEST_ROUTES_DIR.joinpath(test_files[1]), "rb")),
         ("files", open(TEST_ROUTES_DIR.joinpath(test_files[2]), "rb"))
     )
-
     scenario_number = 0
     scenarios = (
-        # params, status, expected_key, name
-        ({SEARCH_FILES_BY_ID: []}, 404, "no_files", "empty choice not found"),  # 1
-        ({SEARCH_FILES_BY_NAME: "???", SEARCH_FILES_BY_ID: [1]}, 403, "no_files", "by name & ids simultaneously"),  # 2
-        ({SEARCH_FILES_BY_NAME: "no exist"}, 404, "no_files", "search files by name no exist"),  # 3
-
-        ({SEARCH_FILES_BY_ID: 1}, 200, "testfiles_3_2", "delete testfile"),  # 2
-        ({SEARCH_FILES_BY_NAME: "testfile2"}, 200, "testfile3", "delete testfile2"),  # 2
-        ({SEARCH_FILES_BY_NAME: "testfile"}, 200, "no_files", "delete files, found by part name"),  # 2
-        # ({CHOICE_FORMAT: json_choice}, 404, "no_files", 55, "search files by id == 55 is not found"),  # 3
-        # ({CHOICE_FORMAT: json_choice}, 200, "testfile2", 2, "testfile2 in json"),  # 4
-        # ({CHOICE_FORMAT: json_choice}, 200, "testfile3", 3, "testfile3 in json"),  # 5
-        # ({CHOICE_FORMAT: files_choice}, 200, "testfile3", 3, "download testfile3"),  # 6
-
-
-        # ({SEARCH_FILES_BY_NAME: "no exist"}, 404, "no_files", "search files by name no exist"),  # 4
-        # ({SEARCH_FILES_BY_NAME: "testfile"}, 200, "all_db_files", "search 3 files by part name"),  # 5
-        # ({SEARCH_FILES_BY_NAME: "testfile3"}, 200, "testfile3", "search 1 file by part name"),  # 6
-        # ({SEARCH_FILES_BY_NAME: f"{now}"}, 200, "all_db_files", "search files by date"),  # 7
-        # ({SEARCH_FILES_BY_ID: [3]}, 200, "testfile3", "search files by id == 3"),  # 8
-        # ({SEARCH_FILES_BY_ID: [3, 2]}, 200, "testfiles_3_2", "search files by id == 1, 2"),  # 9
-        # ({SEARCH_FILES_BY_ID: [3, 55]}, 200, "testfile3", "search files by id == 1, 55"),  # 10
-        # ({SEARCH_FILES_BY_ID: [55]}, 200, "no_files", "search files by id == 55 no exists"),  # 11
+        # params, status, expected_key (files in db in scenario), delete_task_files, delete_suspension_files, name
+        ({SEARCH_FILES_BY_ID: []}, 404, "no_files", False, False, "empty choice not found"),  # 1
+        (
+            {SEARCH_FILES_BY_NAME: "???", SEARCH_FILES_BY_ID: [1]},
+            403, "no_files", False, False, "by name & ids simultaneously"
+        ),  # 2
+        ({SEARCH_FILES_BY_NAME: "no exist"}, 404, "no_files", False, False, "search files by name no exist"),  # 3
+        ({SEARCH_FILES_BY_ID: 1}, 403, "all_db_files", False, False, "can't delete file because of task_files"),  # 4
+        ({SEARCH_FILES_BY_ID: 1}, 403, "all_db_files", True, False, "can't delete because of suspension_files"),  # 5
+        ({SEARCH_FILES_BY_ID: 1}, 200, "testfiles_3_2", False, True, "could delete testfile: no file_records"),  # 6
+        ({SEARCH_FILES_BY_NAME: "testfile2"}, 200, "testfile3", False, False, "delete testfile2"),  # 7
+        ({SEARCH_FILES_BY_NAME: "testfile"}, 200, "no_files", False, False, "delete files, found by part name"),  # 8
     )
-    # - созданы таски todo создать suspension
-    # - с помощью async_db прикрепить файлы к таске и простою  todo
-    # - сделать проверки, что прикрепились и есть  todo
-    # - удалить файл, не прикрепленный  todo
-    # - удалить файл прикрепленный к таске и простою  todo
-    # - к одному из них  todo
-
     async with async_client as ac:
         login_super_user_response = await ac.post(LOGIN, data=super_user_login)
         assert login_super_user_response.status_code == 200, f"User: {super_user_login} couldn't get {LOGIN}"
@@ -530,6 +513,27 @@ async def test_super_user_delete_file_id_url(
         assert uploaded_files_response.status_code == 200, (
             f"{user_orm_login} couldn't get {uploaded_files_response}. Response: {uploaded_files_response.__dict__}"
         )
+        # making task_files_record & suspension_files_record with attached file_id=1
+        task_files_record = TasksFiles(task_id=1, file_id=1)
+        async_db.add(task_files_record)
+        await async_db.commit()
+        await log.ainfo("task_files_record created:", task_files_record=task_files_record)
+        suspension = Suspension(
+            risk_accident=next(iter(json.loads(settings.RISK_SOURCE).values())),  # the first item in dictionary
+            description="_1_[]",
+            suspension_start=datetime.now() - timedelta(days=2),  # CREATE_SUSPENSION_FROM_TIME,
+            suspension_finish=datetime.now() - timedelta(days=1, hours=23, minutes=59),  # CREATE_SUSPENSION_TO_TIME,
+            tech_process=next(iter(json.loads(settings.TECH_PROCESS).values())),  # :int = 25 -first item in dictionary
+            implementing_measures="1",
+            user_id=super_user_orm.id
+        )
+        async_db.add(suspension)
+        await async_db.commit()
+        await log.ainfo("suspension created:", suspension=suspension)
+        suspension_files_record = SuspensionsFiles(suspension_id=1, file_id=1)
+        async_db.add(suspension_files_record)
+        await async_db.commit()
+        await log.ainfo("suspension_files_record created:", suspension_files_record=suspension_files_record)
         # files downloaded:
         file_objects = await async_db.scalars(select(FileAttached))
         file_objects_in_db = file_objects.all()
@@ -540,15 +544,6 @@ async def test_super_user_delete_file_id_url(
         if uploaded_files is not None:
             for file in uploaded_files:
                 assert file in all_files_in_folder, f"Can't find: {file} in files folder: {FILES_DIR}"
-        # expected keys dictionary for search scenarios:
-        expected_key_dictionary = {
-            "all_db_files": files_in_db,
-            "testfiles_3_1": [_ for _ in files_in_db if "testfile2" not in _],
-            "testfile2": [_ for _ in files_in_db if "testfile2" in _],
-            "testfile3": [_ for _ in files_in_db if "testfile3" in _],
-            "testfiles_3_2": [_ for _ in files_in_db if "testfile3" in _ or "testfile2" in _],
-            "no_files": []
-        }
         # checking not admin is forbidden to delete:
         login_user_response = await ac.post(LOGIN, data=user_orm_login)
         assert login_user_response.status_code == 200, f"User: {user_orm_login} couldn't get {LOGIN}"
@@ -558,9 +553,26 @@ async def test_super_user_delete_file_id_url(
             headers={"Authorization": f"Bearer {login_user_response.json()['access_token']}"},
         )
         assert user_response.status_code == 403, f"{user_orm_login} got {test_url + f'{1}'}, :{user_response.__dict__}"
-        # !!!! starting testing scenarios:
-        for params, status, expected_key, name in scenarios:  # todo file_id - лишнее
 
+        # expected keys dictionary for search scenarios:
+        expected_key_dictionary = {
+            "all_db_files": files_in_db,
+            "testfiles_3_1": [_ for _ in files_in_db if "testfile2" not in _],
+            "testfile2": [_ for _ in files_in_db if "testfile2" in _],
+            "testfile3": [_ for _ in files_in_db if "testfile3" in _],
+            "testfiles_3_2": [_ for _ in files_in_db if "testfile3" in _ or "testfile2" in _],
+            "no_files": []
+        }
+        # !!!! starting testing scenarios:
+        for params, status, expected_key, delete_task_files, delete_suspension_files, name in scenarios:
+            if delete_task_files:
+                task_files_records = await async_db.scalar(select(TasksFiles))  # could wanted: .where(file_id == ???)
+                await async_db.delete(task_files_records)
+                await async_db.commit()
+            elif delete_suspension_files:
+                suspension_files_records = await async_db.scalar(select(SuspensionsFiles))  # could wanted: .where(???)
+                await async_db.delete(suspension_files_records)
+                await async_db.commit()
             scenario_file_objects = await async_db.scalars(select(FileAttached))
             scenario_file_objects_in_db = scenario_file_objects.all()
             scenario_files = [_.name for _ in scenario_file_objects_in_db if len(scenario_file_objects_in_db) > 0]
@@ -581,10 +593,11 @@ async def test_super_user_delete_file_id_url(
                     params=params,
                     response=response.json(),
                     status=response.status_code,
+                    task_files_record=task_files_record,
+                    suspension_files_record=suspension_files_record,
                     wings_of_end=f"STATUS: {response.status_code}___ END of SCENARIO: ___ {scenario_number} ___ {name}"
                 )
                 continue
-
             file_objects_after = await async_db.scalars(select(FileAttached))
             file_objects_in_db_after = file_objects_after.all()
             files_names_after = [_.name for _ in file_objects_in_db_after if len(file_objects_in_db_after) > 0]
@@ -592,10 +605,6 @@ async def test_super_user_delete_file_id_url(
                 "total_files_in_db": len(expected_key_dictionary[expected_key]),
                 "files_in_db_after": expected_key_dictionary[expected_key],
                 "scenario_files": set(scenario_files),
-                # "response_attachment": file_object[0].name.split("_")[2],  # "archive.zip"
-                # "expected_key_dictionary": expected_key_dictionary[expected_key],
-                # "file_id": file_object[0].id,
-                # "file_name": file_object[0].name
             }
             match_values = (
                 # name_value, expected_value, exist_value
@@ -607,43 +616,24 @@ async def test_super_user_delete_file_id_url(
                     expected.get("scenario_files") - set(files_names_after),
                     set([_[FILE_NAME] for _ in response.json().get(FILES_UNUSED_IN_DB_REMOVED)])
                 ),
-                # ("Content-disposition: ", expected.get("response_attachment"), response_attachment.split("_")[-1]),
             )
-            # if params.get(CHOICE_FORMAT) == files_choice:
-            #     response_attachment = response.headers.get("content-disposition").split(";")[1]
-            #     response_content_decoded = response._content.decode(errors="ignore")
-            #     match_values = (
-            #         # name_value, expected_value, exist_value
-            #         ("Files in db total: ", expected.get("total_files_in_db"), len(files_in_db)),
-            #         ("Files in db equals uploaded files: ", set(files_in_db), set(uploaded_files)),
-            #         ("Content-disposition: ", expected.get("response_attachment"), response_attachment.split("_")[-1]),
-            #     )
-            # elif params.get(CHOICE_FORMAT) == json_choice:
-            #     match_values = (
-            #         # name_value, expected_value, exist_value
-            #         ("Files in db total: ", expected.get("total_files_in_db"), len(files_in_db)),
-            #         ("Files in db equals uploaded files: ", set(files_in_db), set(uploaded_files)),
-            #         ("File id: ", expected.get("file_id"), response.json()["id"]),
-            #         ("File name: ", expected.get("file_name"), response.json()[FILE_NAME]),
-            #     )
             for name_value, expected_value, exist_value in match_values:
                 assert expected_value == exist_value, f"{name_value} {exist_value} not as expected: {expected_value}"
             await log.awarning(
                 f"SCENARIO: _{scenario_number}_ info: {name}",
                 scenario_files_in_db=scenario_files,
                 files_in_db_after=files_names_after,
-                # response_attachment=response_attachment if params.get(CHOICE_FORMAT) == files_choice else None,
-                # response_content=response_content_decoded if params.get(CHOICE_FORMAT) == files_choice else None,
+                delete_task_files=delete_task_files,
+                delete_suspension_files=delete_suspension_files,
                 params=params,
                 response=response.json(),
-                # response_files=[_[FILE_NAME] for _ in response.json().get(FILES_UNUSED_IN_DB_REMOVED)],
-                # response=response.json() if params.get(CHOICE_FORMAT) == json_choice else None,
-                # uploaded_files=uploaded_files,
-                # login_data=user_orm_login,
+                task_files_records=task_files_record,
+                suspension_files_record=suspension_files_record,
+                login_data=user_orm_login,
                 # response=response.__dict__,
                 wings_of_end=f"_________ END of SCENARIO: ___ {scenario_number}___ {name}"
             )
-        await clean_test_database(async_db, FileAttached, Task, TasksFiles)  # clean db after test todo suspensions
+        await clean_test_database(async_db, FileAttached, Suspension, SuspensionsFiles, Task, TasksFiles)
         await delete_files_in_folder(file_paths)
         all_files_in_folder = [file.name for file in FILES_DIR.glob('*')]
         if uploaded_files is not None:
